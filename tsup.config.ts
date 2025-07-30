58c44d0bf14a0827dd9c85ffac8aedce1807af5c
materwelon <materwelondhruv@gmail.com>
2025-07-30 16:06:02 +0530

    chore: homepage

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,42 +1,48 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig([
-  // ESM build
-  {
-    entry: ['src/index.ts'],
-    platform: 'node',
-    format: ['esm'],
-    skipNodeModulesBundle: true,
-    target: 'es2022',
-    clean: true,
-    shims: true,
-    cjsInterop: false,
-    minify: false,
-    splitting: false,
-    keepNames: true,
-    dts: true,
-    sourcemap: true,
-    treeshake: true,
-    outDir: 'dist',
-    outExtension: () => ({ js: '.mjs', dts: '.d.mts' })
-  },
-  // JS/CJS build
-  {
-    entry: ['src/index.ts'],
-    platform: 'node',
-    format: ['cjs'],
-    skipNodeModulesBundle: true,
-    target: 'es2022',
-    clean: false,
-    shims: true,
-    cjsInterop: true,
-    minify: false,
-    splitting: false,
-    keepNames: true,
-    dts: true,
-    sourcemap: true,
-    treeshake: true,
-    outDir: 'dist',
-    outExtension: () => ({ js: '.js', dts: '.d.ts' })
+import type { Options } from 'tsup';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type, complexity
+function createTsupConfig({
+  format = ['esm', 'cjs'],
+  entry = ['src/index.ts'],
+  dts = true,
+  shims = true,
+  skipNodeModulesBundle = true,
+  clean = true,
+
+  treeshake = true,
+  platform = 'node',
+  target = 'es2022',
+  splitting = false,
+  cjsInterop = format.includes('cjs'),
+  minify = false,
+  keepNames = true,
+  sourcemap = true,
+  outDir = 'dist',
+  outExtension = ({ format }) => {
+    return { js: `.${format === 'esm' ? 'mjs' : 'js'}`, dts: '.d.ts' };
   }
-]);
+}: Options = {}) {
+  return defineConfig({
+    format,
+    entry,
+    dts,
+    shims,
+    skipNodeModulesBundle,
+    clean,
+
+    platform,
+    target,
+    cjsInterop,
+    minify,
+    splitting,
+    keepNames,
+    sourcemap,
+    treeshake,
+    outDir,
+    outExtension
+  });
+}
+
+export default createTsupConfig();
