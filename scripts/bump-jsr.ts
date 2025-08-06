77499dd48d22686ed94a4c6d9e18b07778f18324
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2025-08-06 14:18:38 +0000

    chore: migrate to deno.json

--- scripts/bump-jsr.ts
+++ scripts/bump-jsr.ts
@@ -1,21 +1,24 @@
+import console from "node:console";
 import fs from 'node:fs';
 import { resolve } from 'node:path';
 
-const packagePath = resolve(import.meta.dirname, '../package.json');
+const importMeta = import.meta as { dirname: string }
+
+const packagePath = resolve(importMeta.dirname, '../package.json');
 const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf-8')) as Record<string, unknown>;
 
 if (!packageJson.version) throw new Error('Version not found in package.json');
 
-const jsrConfigPath = resolve(import.meta.dirname, '../jsr.json');
-const jsrJson = JSON.parse(fs.readFileSync(jsrConfigPath, 'utf-8')) as Record<string, unknown>;
+const denoConfigPath = resolve(importMeta.dirname, '../deno.json');
+const denoJson = JSON.parse(fs.readFileSync(denoConfigPath, 'utf-8')) as Record<string, unknown>;
 
-const jsrConfig = {
-  ...jsrJson,
+const denoConfig = {
+  ...denoJson,
   version: packageJson.version
 };
 
-fs.writeFileSync(jsrConfigPath, `${JSON.stringify(jsrConfig, null, 2)}\n`, 'utf-8');
+fs.writeFileSync(denoConfigPath, `${JSON.stringify(denoConfig, null, 2)}\n`, 'utf-8');
 // eslint-disable-next-line no-console
 console.log(
-  `JSR publish version updated from ${jsrJson.version as string} to ${packageJson.version as string} in jsr.json`
+  `JSR publish version updated from ${denoJson.version as string} to ${packageJson.version as string} in deno.json`
 );
